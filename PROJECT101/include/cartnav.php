4c6bab51a8755853949ff607b0b50b85bb44813b
troyeet <159408985+troyeet@users.noreply.github.com>
2024-06-02 14:36:37 +0800

    Add files via upload

---
+++ PROJECT101/include/cartnav.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="product.php">
+                <img src="images/shoe-haven-high-resolution-logo-transparent.png" width="50" height="50" alt="Shoe Haven Logo">
+                Shoe Haven
+            </a>
+            <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="sidebar offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header text-white border-bottom">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Shoe Haven</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+
+                <div class="offcanvas-body" style="margin-left: 15%;">
+                    <form class="d-flex " style="padding-right: 10%;" role="search">
+                        <input class="form-control me-2"  style="width: 500px;" type="search" placeholder="Find your pairs" aria-label="Search">
+                        <button class="btn btn-outline-primary " type="submit">Search</button>
+                    </form>
+                   
+                    <a class="text-decoration-none text-success" href="user_prof.php"><i class="fa-regular fa-circle-user fs-3 mt-1 ps-4"></i></a>
+                </div>
+            </div>
+        </div>
+    </nav>
---
+++ PROJECT101/include/nav.php
@@ -0,0 +1,29 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="product.php">
+                <img src="images/shoe-haven-high-resolution-logo-transparent.png" width="50" height="50" alt="Shoe Haven Logo">
+                Shoe Haven
+            </a>
+            <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="sidebar offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header text-white border-bottom">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Shoe Haven</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+
+                <div class="offcanvas-body pt-1" style="margin-left: 15%;">
+                    <form class="d-flex" style="padding-right: 10%;" role="search" action="search.php" method="GET">
+    <input class="form-control me-2" style="width: 500px" type="search" placeholder="Find your pairs" aria-label="Search" name="query">
+    <button class="btn btn-outline-primary" type="submit">Search</button>
+</form>
+
+                    <a class="text-decoration-none text-success" href="cart.php"><i class="fa-solid fa-cart-shopping fs-3 mt-1"></i></a>
+                    <a class="text-decoration-none text-success" href="user_prof.php"><i class="fa-regular fa-circle-user fs-3 mt-1 ps-4"></i></a>
+                </div>
+            </div>
+        </div>
+    </nav>
